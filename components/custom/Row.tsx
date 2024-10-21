8ebb79719e14231eb2d1f52baea4c4b7e91aab49
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2024-10-21 09:41:03 +0300

    update costom container component

--- components/custom/Row.tsx
+++ components/custom/Row.tsx
@@ -1,12 +1,12 @@
 import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
 
-export default function Container({
+export default function Row({
   children,
   classname,
 }: Readonly<{
   children: ReactNode;
   classname?: string;
 }>) {
-  return <div className={cn("container w-full h-full", classname)}>{children}</div>;
+  return <div className={cn("row", classname)}>{children}</div>;
 }
