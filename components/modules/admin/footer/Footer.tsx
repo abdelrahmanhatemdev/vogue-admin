743ca2c7192345bc1202283cda38e8141be6d4c8
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2024-10-22 05:52:17 +0300

    close mobile sidebar sheet onclick link

--- components/modules/admin/footer/Footer.tsx
+++ components/modules/admin/footer/Footer.tsx
@@ -3,7 +3,7 @@ import ContentContainer from "@/components/custom/ContentContainer";
 
 export default function () {
   return (
-    <footer className="absolute bottom-0 p-3 w-full">
+    <footer className="absolute bottom-0 py-3 w-full">
       <ContentContainer>
         <p>
           <span>2042</span> &#64; Vogue
--- components/modules/admin/sidebar/MobileSidebar.tsx
+++ components/modules/admin/sidebar/MobileSidebar.tsx
@@ -8,7 +8,7 @@ import {
 import { cn } from "@/lib/utils";
 
 import Sidebar from "./Sidebar";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { CiMenuBurger } from "react-icons/ci";
 import Link from "next/link";
 
@@ -17,14 +17,24 @@ export default function MobileSidebar({
 }: Readonly<{
   className?: string;
 }>) {
+  const [open, setOpen] = useState(false);
+
+  const close = () => setOpen(false)
   return (
     <div className={cn("", className)}>
-      <Sheet>
+      <Sheet open={open}>
         <SheetTrigger asChild>
-          <CiMenuBurger className="lg:hidden" />
+          <CiMenuBurger className="lg:hidden" onClick={() => setOpen(true)}/>
         </SheetTrigger>
         <SheetContent side="left">
-          <Sidebar />
+        <ul className="p-4 border-e border-solid border-stone-800 w-full">
+          <li>
+            <Link href="/admin" className="font-bold" onClick={close}>Dashboard</Link>
+          </li>
+          <li>
+            <Link href="/admin/users" className="font-bold" onClick={close}>Users</Link>
+          </li>
+        </ul>
         </SheetContent>
       </Sheet>
     </div>
