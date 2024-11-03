ba353781af8abc2175c479a6695c853437991877
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2024-11-03 20:51:26 +0200

    fix modal peoplem

--- components/custom/Modal.tsx
+++ components/custom/Modal.tsx
@@ -11,30 +11,27 @@ export type ModalState = {
   title: ReactNode;
   description: ReactNode;
   children: ReactNode;
-};
+}
 
 import { Dispatch, memo, ReactNode, SetStateAction } from "react";
+
 
 const Modal = memo(function Modal({
   title,
   description,
   children,
-  modalOpen,
-  setModalOpen,
+  modalOpen, 
+  setModalOpen
 }: {
   title: ModalState["title"];
   description: ModalState["description"];
   children?: ModalState["children"];
   modalOpen?: boolean;
-  setModalOpen: Dispatch<SetStateAction<boolean>>;
+  setModalOpen: Dispatch<SetStateAction<boolean>>
 }) {
   return (
     <Dialog open={modalOpen} onOpenChange={setModalOpen}>
-      <DialogContent
-        asChild
-        onPointerDownOutside={() => setModalOpen(false)}
-        aria-describedby={undefined}
-      >
+      <DialogContent onPointerDownOutside={() => setModalOpen(false)} aria-describedby={undefined}>
         <DialogHeader>
           <DialogTitle>{title}</DialogTitle>
         </DialogHeader>
@@ -43,6 +40,6 @@ const Modal = memo(function Modal({
       </DialogContent>
     </Dialog>
   );
-});
+})
 
-export default Modal;
+export default Modal
