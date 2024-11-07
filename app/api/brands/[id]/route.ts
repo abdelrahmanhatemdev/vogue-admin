2bd23adc9cca05d3a19186c187724d0186ee04f2
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2024-11-07 13:21:22 +0200

    update brand routes

--- app/api/brands/[id]/route.ts
+++ app/api/brands/[id]/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { getDoc, doc } from "firebase/firestore";
 import { collectoinName } from "../route";
 import { db } from "@/firebase.config";
-import { getCategories } from "@/actions/Brand";
+import { getBrands} from "@/actions/Brand";
 
 export const dynamic = 'force-static'
 
@@ -26,7 +26,7 @@ export async function GET(req: Request, props: { params: Promise<{ id: string }>
 }
 
 export async function generateStaticParams() {
-  const list: Brand[] = await getCategories();
+  const list: Brand[] = await getBrands();
 
   return list.map(({ id }: { id: string }) => ({id}));
 }
