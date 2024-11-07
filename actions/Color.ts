8557e6f5b49d841f7c2157db1000442fe186fdf3
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2024-11-07 19:38:37 +0200

    add colors actions

---
+++ actions/Color.ts
@@ -0,0 +1,97 @@
+"use server";
+import axios from "axios";
+import { revalidateTag } from "next/cache";
+
+const apiURL = `${process.env.NEXT_PUBLIC_APP_API}/colors`;
+const tag: string = "colors";
+
+export const getColors = async () => {
+  try {
+    const res = await fetch(apiURL, {
+      next: { tags: [tag] },
+      cache: "force-cache",
+    });
+    let data: Size[] = [];
+
+    if (res) {
+      const { data } = await res.json();
+
+      const sortedData = data.sort((a: Size, b: Size) =>
+        b.updatedAt.localeCompare(a.updatedAt)
+      );
+
+      return sortedData;
+    }
+    return data;
+  } catch (error) {
+    return console.log(error);
+  }
+}
+
+export async function getSizeById(id: string) {
+  try {
+    const res = await fetch(`${apiURL}/${id}`, {
+      next: { tags: [tag] },
+      cache: "force-cache",
+    });
+
+    const { data } = await res.json();
+    return data;
+  } catch (error) {
+    return console.log(error);
+  }
+}
+
+export async function addSize(data: Partial<Size>) {
+  return axios
+    .post(apiURL, data)
+    .then((res) => {
+      if (res?.statusText === "OK" && res?.data?.message) {
+        revalidateTag(tag);
+        return { status: "success", message: res.data.message };
+      }
+      if (res?.data?.error) {
+        return { status: "error", message: res.data.error };
+      }
+    })
+    .catch((error) => {
+      const message = error?.response?.data?.error || "Something Wrong";
+      return { status: "error", message };
+    });
+}
+
+export async function editSize(data: Partial<Size>) {
+  return axios
+    .put(apiURL, data)
+    .then((res) => {
+      if (res?.statusText === "OK" && res?.data?.message) {
+        revalidateTag(tag);
+        return { status: "success", message: res.data.message };
+      }
+      if (res?.data?.error) {
+        return { status: "error", message: res.data.error };
+      }
+    })
+    .catch((error) => {
+      const message = error?.response?.data?.error || "Something Wrong";
+      return { status: "error", message };
+    });
+}
+
+export async function deleteSize(data: { id: string }) {
+  return axios
+    .delete(apiURL, { data })
+    .then((res) => {
+      if (res?.statusText === "OK" && res?.data?.message) {
+        revalidateTag(tag);
+        return { status: "success", message: res.data.message };
+      }
+      if (res?.data?.error) {
+        return { status: "error", message: res.data.error };
+      }
+    })
+    .catch((error) => {
+      const message = error?.response?.data?.error || "Something Wrong";
+      return { status: "error", message };
+    });
+}
