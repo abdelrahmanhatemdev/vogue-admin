5baa737677a8c1531f70d8feb9368121cd7b02bb
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2025-01-19 13:11:43 +0200

    add Label Schema

---
+++ lib/validation/labelSchema.ts
@@ -0,0 +1,16 @@
+import z from "zod";
+
+export const LabelSchema = z.object({
+  uuid: z.string().uuid({ message: "Invalid UUID format." }),
+  name: z
+    .string()
+    .min(1, {
+      message: "Name is required",
+    })
+    .max(9, {
+      message: "Name should not have more than 9 charachters.",
+    }),
+  hex: z.string().min(1, {
+    message: "Hex code is required",
+  }),
+});
