38b62d80df7ea3857e24ad15b70feeb20b9fb9b4
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2025-01-19 12:54:30 +0200

    update theme toggle style

--- components/custom/ToggleTheme.tsx
+++ components/custom/ToggleTheme.tsx
@@ -1,9 +1,9 @@
 "use client";
 import useTheme from "@/hooks/useTheme";
-import { Switch } from "@/components/ui/costom-switch";
 import { IoSunnyOutline } from "react-icons/io5";
 import { IoMoonSharp } from "react-icons/io5";
 import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
 
 function ToggleTheme() {
   const { theme, toggleTheme } = useTheme();
@@ -17,24 +17,25 @@ function ToggleTheme() {
 
   return (
     <div className="relative w-14 h-7" title="Theme">
-      <div
-        className="absolute inset-0 w-full h-full flex items-center justify-between cursor-pointer px-[.30rem]"
+      <Button
         onClick={toggleTheme}
-      >
-        <div className="bg-sky-600 text-sky-50 rounded-full p-[.2rem]">
-          <IoMoonSharp size={14} className={`${theme === "dark" ? "block" : "hidden"}`}/>
-        </div>
-        <div className="bg-yellow-200 text-yellow-700 rounded-full p-[.2rem]">
-          <IoSunnyOutline size={14}  className={`${theme !== "dark" ? "block" : "hidden"}`}/>
-        </div>
-      </div>
-      <Switch
-        checked={theme === "dark" ? true : false}
-        onCheckedChange={toggleTheme}
-        className="w-14 h-7 bg-sky-600"
-        id="theme-switch"
+        className=" rounded-full"
+        variant={"nostyle"}
         aria-label="Theme Switch"
-      />
+        title="Switch Theme"
+      >
+        {theme !== "dark" && <div className="bg-sky-600 text-sky-50 rounded-full p-1">
+          <IoMoonSharp
+            size={14}
+          />
+        </div>}
+        {theme === "dark" && <div className="bg-neutral-950 text-yellow-500 rounded-full p-1">
+          <IoSunnyOutline
+            size={14}
+          />
+        </div>}
+        
+      </Button>
     </div>
   );
 }
