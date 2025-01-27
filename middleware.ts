9683230b31756359cb0bd4f9b3f95637ca8a72ff
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2025-01-27 17:02:01 +0200

    rateLimiter

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,29 @@
+import { RateLimiterMemory } from "rate-limiter-flexible";
 
 import { NextResponse, type NextRequest } from "next/server";
 
-export async function middleware() {
-   return NextResponse.next();
+const rateLimiter = new RateLimiterMemory({
+  points: 10, 
+  duration: 1,
+});
+
+export async function middleware(req: NextRequest) {
+  const ip =
+    req.headers.get("x-real-ip") ||
+    req.headers.get("x-forwarded-for") ||
+    req.headers.get("host") ||
+    "unknown-ip";
+
+  try {
+    await rateLimiter.consume(ip);
+    return NextResponse.next();
+  } catch (err) {
+    return new Response("Too many requests, please try again later.", {
+      status: 429,
+    });
+  }
 }
 
 export const config = {
-  matcher: [
-    "/:path*",
-  ],
+  matcher: ["/:path*"],
 };
