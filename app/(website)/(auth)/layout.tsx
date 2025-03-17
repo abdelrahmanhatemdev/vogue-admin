b30a86bcc0b16277da6a0ab92895683b30f84f42
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2025-03-17 17:11:56 +0200

    redirect to portfolio link in auth interface

--- app/(website)/(auth)/layout.tsx
+++ app/(website)/(auth)/layout.tsx
@@ -33,9 +33,9 @@ export default async function layout({
           <AuthSwiper />
           <div className="flex items-center absolute inset-5 justify-between h-fit p-2">
             <Logo className="z-10" />
-            <Link href={`/`} className="z-10 ">
+            <Link href={`https://www.hatem.bio/`} className="z-10 ">
               <span className="bg-[hsla(0,0%,0%,10%)] text-neutral-50 text-xs rounded-full p-2 hover:bg-[hsla(0,0%,0%,30%)] transition-colors shadow-sm">
-                Back to Website
+                hatem.bio
               </span>
             </Link>
           </div>
